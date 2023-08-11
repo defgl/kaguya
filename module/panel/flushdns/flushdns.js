c27443078d756f3bec38f2acc6599c803aa68f36
anyawa <126152485+anyawa@users.noreply.github.com>
2023-08-11 18:40:34 +0800

    modified:   module/panel/flushdns/flushdns.js 	modified:   module/panel/flushdns/flushdns.sgmodule

--- module/panel/flushdns/flushdns.js
+++ module/panel/flushdns/flushdns.js
@@ -1,61 +1,65 @@
-;(async () => {
-  let titlecontent = await fetchtitlecontent()
-  let panel = { title: titlecontent }
-  let showServer = true
-  let dnsCache
-  
-  if (typeof $argument !== "undefined") {
-    let arg = Object.fromEntries($argument.split("&").map((item) => item.split("=")))
-    if (arg.title) panel.title = arg.title
-    if (arg.icon) panel.icon = arg.icon
-    if (arg.color) panel["icon-color"] = arg.color
-    if (arg.server === "false") showServer = false
+/*
+ * 由@zZPiglet编写
+ * 原脚本地址：https://raw.githubusercontent.com/zZPiglet/Task/master/asset/flushDNS.js
+ * 由@Rabbit-Spec修改
+ * 更新日期：2022.06.17
+ * 版本：1.6
+ */
+
+!(async () => {
+  let showServer = true,
+      dnsCache;
+  if (typeof $argument != "undefined") {
+      let arg = Object.fromEntries($argument.split("&").map((item) => item.split("=")));
+      if (arg.title) panel.title = arg.title;
+      if (arg.icon) panel.icon = arg.icon;
+      if (arg.color) panel["icon-color"] = arg.color;
+      if (arg.server == "false") showServer = false;
   }
-  
   if (showServer) {
-    dnsCache = (await httpAPI("/v1/dns", "GET")).dnsCache
-    dnsCache = [...new Set(dnsCache.map((d) => d.server))].toString().replace(/,/g, "\n")
+      dnsCache = (await httpAPI("/v1/dns", "GET")).dnsCache;
+      dnsCache = [...new Set(dnsCache.map((d) => d.server))].toString().replace(/,/g, "\n");
   }
-  
-  if ($trigger === "button") await httpAPI("/v1/dns/flush")
-  
-  let delay = ((await httpAPI("/v1/test/dns_delay")).delay * 1000).toFixed(0)
-  panel.content = `Ping DNS: ${delay}ms${dnsCache ? `\nserver:\n${dnsCache}` : ""}`
-  
-  $done(panel)
-})()
+  if ($trigger == "button") await httpAPI("/v1/dns/flush");
+  let delay = ((await httpAPI("/v1/test/dns_delay")).delay * 1000).toFixed(0);
+
+  let titlecontent = await fetchtitlecontent();
+
+  if ($trigger == "button") await httpAPI("/v1/profiles/reload");
+
+  $done({
+    title: titlecontent,
+    content: `Flush: ${delay}ms${dnsCache ? `\nserver:\n${dnsCache}` : ""}`,
+    icon: params.icon,
+    "icon-color": params.color
+  });
+
+})();
 
 function httpAPI(path = "", method = "POST", body = null) {
   return new Promise((resolve) => {
-    $httpAPI(method, path, body, (result) => {
-      resolve(result)
-    })
-  })
+      $httpAPI(method, path, body, (result) => {
+          resolve(result);
+      });
+  });
 }
 
-function fetchtitlecontent() {
+async function fetchtitlecontent() {
   return new Promise((resolve, reject) => {
-    let url = 'https://zj.v.api.aa1.cn/api/wenan-shici/?type=json'
+    let url = 'https://zj.v.api.aa1.cn/api/wenan-shici/?type=json';
     $httpClient.get(url, function(error, response, data) {
       if (error) {
-        reject(`error: ${error.message}`)
-        return
+        reject(`error: ${error.message}`);
+        return;
       }
-      
       if (response.status !== 200) {
-        reject(`failed to fetch data. http status: ${response.status}`)
-        return
+        reject(`failed to fetch data. http status: ${response.status}`);
+        return;
       }
-      
-      let regex = /━━━━━━━━━\n([\s\S]*?)\n━━━━━━━━━/
-      let match = regex.exec(data)
-      
-      if (match) {
-        let extractedtext = match[1]
-        resolve(extractedtext)
-      } else {
-        reject(`failed to extract data from response`)
-      }
-    })
-  })
+      let jsondata = JSON.parse(data);
+      let fulltext = jsondata.msg;
+      let extractedtext = fulltext.split("。——")[0] + "。";
+      resolve(extractedtext);
+    });
+  });
 }
