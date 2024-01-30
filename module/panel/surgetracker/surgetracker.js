84ad9c5b62a6e8d2c02827d1417b890bf367d6d8
ANYA <126152485+iyqgrl@users.noreply.github.com>
2024-01-30 09:44:33 +0000

    30 Jan 2024, 17:44

--- module/panel/surgetracker/surgetracker.js
+++ module/panel/surgetracker/surgetracker.js
@@ -6,6 +6,8 @@
  * 版本：1.5
 */
 
+let params = getParams($argument)
+
 !(async () => {
 /* 时间获取 */
 let traffic = (await httpAPI("/v1/traffic","GET"))
@@ -17,38 +19,52 @@ if ($trigger == "button") await httpAPI("/v1/profiles/reload");
 
   $done({
       title:"Surge Pro®",
-      content:`已啟動: ${formattedTime}`,
-      icon: "rotate.3d.fill", // 直接写入图标样式
-      "icon-color": "#318ce7" // 直接写入图标颜色
+      content:`已啟動: ${startTime}`,
+		icon: params.icon,
+		"icon-color":params.color
     });
 
 })();
 
-function timeTransform(dateNow, dateTime) {
-  let dateDiff = dateNow - dateTime;
-  let days = Math.floor(dateDiff / (24 * 3600 * 1000)); // 计算相差天数
-  let leave1 = dateDiff % (24 * 3600 * 1000);           // 计算天数后剩余的毫秒数
-  let hours = Math.floor(leave1 / (3600 * 1000));       // 计算出小时数
-  let leave2 = leave1 % (3600 * 1000);                  // 计算小时数后剩余的毫秒数
-  let minutes = Math.floor(leave2 / (60 * 1000));       // 计算相差分钟数
-  let leave3 = leave2 % (60 * 1000);                    // 计算分钟数后剩余的毫秒数
-  let seconds = Math.round(leave3 / 1000);              // 计算相差秒数
+function timeTransform(dateNow,dateTime) {
+let dateDiff = dateNow - dateTime;
+let days = Math.floor(dateDiff / (24 * 3600 * 1000));//计算出相差天数
+let leave1=dateDiff%(24*3600*1000)    //计算天数后剩余的毫秒数
+let hours=Math.floor(leave1/(3600*1000))//计算出小时数
+//计算相差分钟数
+let leave2=leave1%(3600*1000)    //计算小时数后剩余的毫秒数
+let minutes=Math.floor(leave2/(60*1000))//计算相差分钟数
+//计算相差秒数
+let leave3=leave2%(60*1000)      //计算分钟数后剩余的毫秒数
+let seconds=Math.round(leave3/1000)
 
-  let formattedTime = [
-      days > 0 ? `${days}天` : '',
-      hours.toString().padStart(2, '0'),
-      minutes.toString().padStart(2, '0'),
-      seconds.toString().padStart(2, '0')
-  ].filter(Boolean).join(':');
+if(days==0){
 
-  return formattedTime;
+	if(hours==0){
+	if(minutes==0)return(`${seconds}秒`);
+	return(`${minutes}分${seconds}秒`)
+	}
+	return(`${hours}时${minutes}分${seconds}秒`)
+	}else {
+	return(`${days}天${hours}时${minutes}分`)
+	}
+
 }
 
 
 function httpAPI(path = "", method = "POST", body = null) {
-  return new Promise((resolve) => {
-      $httpAPI(method, path, body, (result) => {
-          resolve(result);
-      });
-  });
+    return new Promise((resolve) => {
+        $httpAPI(method, path, body, (result) => {
+            resolve(result);
+        });
+    });
+}
+
+function getParams(param) {
+  return Object.fromEntries(
+    $argument
+      .split("&")
+      .map((item) => item.split("="))
+      .map(([k, v]) => [k, decodeURIComponent(v)])
+  );
 }
