15948b5eb97302036d598efaefd971b44a752e68
ANYA <126152485+iyqgrl@users.noreply.github.com>
2024-01-30 17:09:36 +0800

    modified:   module/panel/surgetracker/surgetracker.js

--- module/panel/surgetracker/surgetracker.js
+++ module/panel/surgetracker/surgetracker.js
@@ -16,12 +16,6 @@
   // 打印原始的 startTime
   console.log("Original startTime: ", startTime);
 
-  // 字体转换
-  startTime = transformFont(startTime, TABLE, INDEX);
-
-  // 打印转换后的 startTime
-  console.log("Transformed startTime: ", startTime);
-
   let titlecontent = await fetchtitlecontent();
 
   if ($trigger == "button") await httpAPI("/v1/profiles/reload");
@@ -93,24 +87,4 @@ function httpAPI(path = "", method = "POST", body = null) {
       resolve(result);
     });
   });
-}
-
-// 字体表
-const TABLE = {
-  "monospace-regular": ["𝟶", "𝟷", "𝟸", "𝟹", "𝟺", "𝟻", "𝟼", "𝟽", "𝟾", "𝟿", "𝚊", "𝚋", "𝚌", "𝚍", "𝚎", "𝚏", "𝚐", "𝚑", "𝚒", "𝚓", "𝚔", "𝚕", "𝚖", "𝚗", "𝚘", "𝚙", "𝚚", "𝚛", "𝚜", "𝚝", "𝚞", "𝚟", "𝚠", "𝚡", "𝚢", "𝚣", "𝙰", "𝙱", "𝙲", "𝙳", "𝙴", "𝙵", "𝙶", "𝙷", "𝙸", "𝙹", "𝙺", "𝙻", "𝙼", "𝙽", "𝙾", "𝙿", "𝚀", "𝚁", "𝚂", "𝚃", "𝚄", "𝚅", "𝚆", "𝚇", "𝚈", "𝚉"],
-};
-
-// 索引对象
-const INDEX = {};
-for (let i = 48; i <= 57; i++) INDEX[i] = i - 48; // 数字 0-9
-for (let i = 65; i <= 90; i++) INDEX[i] = i - 65 + 36; // 大写字母 A-Z
-for (let i = 97; i <= 122; i++) INDEX[i] = i - 97 + 10; // 小写字母 a-z
-
-// 字体转换函数
-function transformFont(str, table, index) {
-  return [...(str || '')].map(c => {
-    const code = c.charCodeAt(0).toString();
-    const idx = index[code];
-    return table["monospace-regular"][idx] || c;
-  }).join('');
 }
