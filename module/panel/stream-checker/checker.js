6305e1c857633eea470b7f2111bd12be89b57817
G.R.L <126152485+iyqgrl@users.noreply.github.com>
2024-03-26 11:39:03 +0800

    modified:   module/panel/stream-checker/checker.js

--- module/panel/stream-checker/checker.js
+++ module/panel/stream-checker/checker.js
@@ -502,7 +502,7 @@ const REQUEST_HEADERS = {
 		let from_who = jsonData.from_who;
 		let result = `${hitokoto} - 《${from}》 -`;
 		if (from_who) {
-		  result = `📒： ${hitokoto} - ${from_who}《${from}》 -`;
+		  result = ` 📒：${hitokoto} - ${from_who}《${from}》 -`;
 		}
 		resolve(result);
 	  });
@@ -2362,7 +2362,7 @@ const REQUEST_HEADERS = {
 	}
   
 	console.log(`${title} | ${rating}\n${currentDate} | ${comment}`);
-	return ` 📆： ${currentDate} | ✨ ${rating}\n 🎬：${title}\n 📗：${comment}`;
+	return ` 📆：${currentDate} | ✨ ${rating}\n 🎬：${title}\n 📗：${comment}`;
   }
   
   function timeout(delay = 5000) {
